ddac091d296bbb6d2a264b929ec9e4b05aca285d
Michael Flach <vuti@gmx.de>
2019-02-04 03:06:40 +0100

    DB issue test

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
-if(process.env.NODE_ENV === 'development') {
+if(process.env.NODE_ENV === 'production'){
+	module.exports = {mongoURI: 'mongodb://heroku_2gg0mq35:32o53gm826fotcpmtrbm9qqcmi@ds221115.mlab.com:21115/heroku_2gg0mq35'}
+} else {
 	module.exports = {mongoURI: 'mongodb://localhost/vidjot'}
-	// module.exports = {mongoURI: 'mongodb://heroku_2gg0mq35:32o53gm826fotcpmtrbm9qqcmi@ds221115.mlab.com:21115/heroku_2gg0mq35'}
 }
+
