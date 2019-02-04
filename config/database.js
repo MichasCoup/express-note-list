077199ef7aedc8c52b8852901c415c8477cbf009
Michael Flach <vuti@gmx.de>
2019-02-04 03:03:12 +0100

    DB issue test

---
+++ config/database.js
@@ -0,0 +1,4 @@
+if(process.env.NODE_ENV === 'development') {
+	module.exports = {mongoURI: 'mongodb://localhost/vidjot'}
+	// module.exports = {mongoURI: 'mongodb://heroku_2gg0mq35:32o53gm826fotcpmtrbm9qqcmi@ds221115.mlab.com:21115/heroku_2gg0mq35'}
+}
